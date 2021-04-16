ce7d050dcebb7ac50cd386e3322404bb0b71defd
jhonalex1199 <2050@holbertonschool.com>
2021-04-15 22:55:43 -0500

    c

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,23 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static.
+# sets up your web servers for the deployment of web_static
 
+sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "
-<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
+</html>" | sudo tee /data/web_static/releases/test/index.html
+# symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-printf %s "
-server {
-    listen 80;
-    listen [::]:80 default_server;
-    server_name https://khawla.tech/
-    root   /var/www//html;
-    index index.nginx-debian.html index.nginx-debian.html;
-    add_header X-Served-By 1391-web-01-1596560744;
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    location /hbnb_static {
-        alias  /data/web_static/current;
-    }
-}" > /etc/nginx/sites-available/default
+sudo sed -i '37 i \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default 
 sudo service nginx restart
